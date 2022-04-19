6bc1175f5381d8a7e5d0a2400390ca8534135540
Shamimgardobuya <shamimobuya@gmail.com>
2022-04-20 00:07:00 +0300

    did append

--- locker.py
+++ locker.py
@@ -1,10 +1,21 @@
 a=[]
-t=input({a})
-total=sum(a)
-if sum(a) ==3:
-    print("Locker is full,don't put more")
-elif sum(a)<3 :
-    print("Can add one more ")
+#locker 2 and locker 3 are full,only locker one has remained,
+#a locker takes only 3 books,if 3
+# trying to enable one to add numbers in  the list . 
+        #  for p in range(1 and 10)
+
+    p= int(input("Enter integer"))
+    
+    if (p<3):
+      a.append(p)
+    elif (p>3):
+      print("Can't add anymore,locker is full")      
+  
+             
+# if sum(a) ==3:
+#     print("Locker is full,don't put more")
+# elif sum(a)<3 :
+#     print("Can add one more ")
     
 
 
