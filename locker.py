4fa3c217cb1a69fad38e35578ed2d69b768eb5d8
Shamimgardobuya <shamimobuya@gmail.com>
2022-04-23 01:11:22 +0300

    input in list

--- locker.py
+++ locker.py
@@ -2,14 +2,19 @@ a=[]
 #locker 2 and locker 3 are full,only locker one has remained,
 #a locker takes only 3 books,if 3
 # trying to enable one to add numbers in  the list . 
-        #  for p in range(1 and 10)
+p=int(input("Enter list of numbers: ", ))
+a.append(p)
+if sum(a)>3:
+   print(f"list is{a} cannot add more")
+elif sum(a)<3:
+         print(f"list is {a}Can add more bag")
+elif sum(a):
+             print(f"Locker is full")
 
-    p= int(input("Enter integer"))
-    
-    if (p<3):
-      a.append(p)
-    elif (p>3):
-      print("Can't add anymore,locker is full")      
+    # if (p<3):
+    #   a.append(p)
+    # elif (p>3):
+    #   print("Can't add anymore,locker is full")      
   
              
 # if sum(a) ==3:
